94a4c3221f722285a3aa34697dfa1842f5bf43eb
Joel <joel.d.hart1@gmail.com>
2021-03-14 19:49:52 +0000

    another edit

--- iss_promised.js
+++ iss_promised.js
@@ -1,19 +1,30 @@
-// iss_promised.js
 
-// ...
+const request = require('request-promise-native');
+const { parse } = require('request/lib/cookies');
 
-/* 
- * Input: None
- * Returns: Promise for fly over data for users location
- */
-const nextISSTimesForMyLocation = function() {
-  return fetchMyIP()
-    .then(fetchCoordsByIP)
-    .then(fetchISSFlyOverTimes)
-    .then((data) => {
-      const { response } = JSON.parse(data);
-      return response;
-    });
+const fetchMyIP = function() {
+  return request('https://api.ipify.org?format=json')
+}
+
+const fetchCoordsByIP = function(body) {
+  const ip = JSON.parse(body).ip
+  return request('https://freegeoip.app/json/' + ip)
 };
+
+const fetchISSFlyOverTimes = function(body) {
+  const latitude = JSON.parse(body).latitude
+  const longitude = JSON.parse(body).longitude
+  return request(`http://api.open-notify.org/iss-pass.json?lat=${latitude}&lon=${longitude}`)
+}
+
+const nextISSTimesForMyLocation = function () {
+  return fetchMyIP()
+  .then(fetchCoordsByIP)
+  .then(fetchISSFlyOverTimes)
+  .then(data => {
+    const response = JSON.parse(data);
+    return response;
+  })
+}
 
 module.exports = { nextISSTimesForMyLocation };
